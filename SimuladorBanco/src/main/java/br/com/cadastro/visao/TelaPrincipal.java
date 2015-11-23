92ea70040a18ff108c60f0606caa9c8f18c5fd3d
GabrielZN <GabrielZN@192.168.47.240>
2015-11-23 09:32:19 -0200

    Criando as classes.

---
+++ SimuladorBanco/src/main/java/br/com/cadastro/visao/TelaPrincipal.java
@@ -0,0 +1,50 @@
+package br.com.cadastro.visao;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import javax.swing.JTextPane;
+import java.awt.FlowLayout;
+
+public class TelaPrincipal extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TelaPrincipal frame = new TelaPrincipal();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public TelaPrincipal() {
+		setTitle("BEM VINDO !");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 747, 488);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout(0, 0));
+	}
+
+	public JTextPane getTextPane() {
+		return getTextPane();
+	}
+}
